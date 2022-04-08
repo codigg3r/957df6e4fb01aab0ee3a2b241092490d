51159510e172b82f6302468c845aa186be8b17f0
Sukru Polat <sukru@polat.me>
2022-04-08 22:20:16 +0300

    Intial commit

---
+++ app/src/main/java/me/sukru/carpettravel/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.sukru.carpettravel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(true)
+            println("Hello World")
+    }
+}
